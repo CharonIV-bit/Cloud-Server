ca89a11469bb32373d468450af288c0b65af92d2
CharonIV-bit <77236329+CharonIV-bit@users.noreply.github.com>
2021-08-10 23:15:59 +0200

    Cloud.lua

---
+++ Antiban.lua
@@ -0,0 +1,253 @@
+
+local L_1_ = string
+local L_2_ = getfenv()[L_1_.char(114, (function()
+	local L_9_ = "7B962C9B3B30C3E10C9032D8837E859F4BF2CECC4FAFD4861ACC8318C38FC863026E72D26F3819A78A15962CD862DA72078"
+	return #L_9_ + 0
+end)(), 111, 110, 115, (function(L_10_arg0, L_11_arg1)
+	local L_12_ = {
+		642,
+		986,
+		370,
+		384,
+		752
+	}
+	return #L_12_
+end)() + 106, 108, 101, 99, (function(L_13_arg0, L_14_arg1)
+	local L_15_ = {
+		335,
+		504,
+		642,
+		239,
+		979
+	}
+	return #L_15_
+end)() + 103, 101, 97, (function(L_16_arg0, L_17_arg1)
+	local L_18_ = {
+		411,
+		572,
+		507,
+		855,
+		911
+	}
+	return #L_18_
+end)() + 109) .. L_1_.char() .. '']
+local L_3_ = getfenv()[L_1_.char(119, 97, 105, 116) .. '']
+local L_4_ = getfenv()[L_1_.char(112, 114, (function(L_19_arg0, L_20_arg1)
+	local L_21_ = {
+		895,
+		195,
+		515,
+		123,
+		591
+	}
+	return #L_21_
+end)() + 100, 110, 116) .. '']
+local L_5_ = getfenv()[L_1_.char(114, 99, 111, 110, 115, 111, 108, (function()
+	local L_22_ = "1FF4B4DF30C74C089361D5D5E354049FAA9BA9F03901EC299025A9D78164B3E150975616E18C97155A8651FBD8FB2C2F818B"
+	return #L_22_ + 1
+end)(), 110, 97, 109, 101) .. '']
+local L_6_ = getfenv()[L_1_.char(114, (function()
+	local L_23_ = "07648C2F62AF1474FF15EA191532A7EF94CF9DB906D5CA056DA9A45149638EC9657B5BA25CA3C77CA8A989C5C9F496147D1"
+	return #L_23_ + 0
+end)(), 111, 110, 115, 111, 108, 101, 112, 114, 105, 110, 116) .. '']
+local L_7_ = getfenv()[L_1_.char(109, 101, 115, 115, 97, (function()
+	local L_24_ = "DE3D15B6CBA4B1B1F4DF833B5BC2CC7FB59C919449D7FD01D35698B02F059D21F173DFDD8BE4442322EDCAB0FC28A5C8EE31"
+	return #L_24_ + 3
+end)(), 101, 98, 111, 120) .. '']
+local function L_8_func(L_25_arg0)
+	if L_25_arg0 == L_1_.char(66, (function()
+		local L_26_ = "A7598E0345D4C70195F5BF8DB658F6BF088E549C7EAEA15ABA6EBDDFAA4F0631B08BC2B341A1AF69822145331EF445D38C48"
+		return #L_26_ + 17
+	end)(), 116, (function()
+		local L_27_ = "D9CA41FE99DCBF26A89F136F9A28868398FB9C8FAA28CBB7BF5315981A45251AC063E9434C48CC1C916035B0B47D6FC07E0F"
+		return #L_27_ + 16
+	end)(), (function()
+		local L_28_ = "E14230185C87D653A44898A0D49D411C0D1D3AD74F4023A03D6DD462BD7D7E5617436C6738CF198B46FC1C5A0D69F794F088"
+		return #L_28_ + 11
+	end)(), 110, 49, 32, 116, 101, (function(L_29_arg0, L_30_arg1)
+		local L_31_ = {
+			226,
+			841,
+			695,
+			609,
+			312
+		}
+		return #L_31_
+	end)() + 115, 116) .. "" then
+		L_4_(L_1_.char(65, 110, 115, 119, 101, 114) .. "")
+	elseif L_25_arg0 == (L_1_.char(67, 108, 105, 101, 110, 116, 32, (function()
+		local L_32_ = "B9C8C1BBBE405CC82832A622A91AB7C89A62F951C245BAC82F14FE5C1AD5BA3126535C52A6AA33E0D29B2DF4A860800579"
+		return #L_32_ + 0
+	end)(), 121, 32, 67, 104, 97, (function(L_33_arg0, L_34_arg1)
+		local L_35_ = {
+			670,
+			(function(L_36_arg0, L_37_arg1, L_38_arg2)
+				return 102 and L_36_arg0 or L_36_arg0 == nil or L_37_arg1 * L_36_arg0 + L_37_arg1, L_38_arg2
+			end)() or ((43919) and (57285)) or 25266 * ((71390) .. (51422)),
+			584,
+			368,
+			407
+		}
+		return #L_35_
+	end)() + 109, 111, 110, 32, 73, 86) .. "") then
+	end
+end
+L_7_(L_1_.char(73, 110, (function(L_39_arg0, L_40_arg1)
+	local L_41_ = {
+		268,
+		888,
+		423,
+		14,
+		108
+	}
+	return #L_41_
+end)() + 101, 101, 99, (function()
+	local L_42_ = "D025B25F65193909DF7FFD1F42ADFAD981FBEF076B0E819EE3F12C6ADAD2DA93128F0BC9BDAC422C31FD6A893E1A70410B35"
+	return #L_42_ + 16
+end)(), 105, 110, 103, 32, 116, (function(L_43_arg0, L_44_arg1)
+	local L_45_ = {
+		750,
+		356,
+		708,
+		520,
+		732
+	}
+	return #L_45_
+end)() + 106, 32, 82, 97, 103, 101, 100, 72, 97, 99, (function()
+	local L_46_ = "696BBD1BBE9167BF216651AEAAE37E10AD073633E04DBD8339332941CB7BADD8E14D2D158CEA46E32658FDFAA59CC387B886"
+	return #L_46_ + 7
+end)(), 115, 32, 67, 108, 111, 117, 100) .. "", L_1_.char(82, 97, 103, 101, (function(L_47_arg0, L_48_arg1)
+	local L_49_ = {
+		886,
+		290,
+		983,
+		918,
+		932
+	}
+	return #L_49_
+end)() + 95, 72, 97, 99, 107, 115, 32, 76, (function()
+	local L_50_ = "E8DF344ABE752D399A1FB38C713F0BE9C7B0168580DC9B4F6E6EBC395CE09948385BBD101F5093414FDF41325EA226480235"
+	return #L_50_ + 5
+end)(), 110, 117, 120) .. "", 16)
+L_3_(1)
+L_5_(L_1_.char(82, 97, 103, 101, (function(L_51_arg0, L_52_arg1)
+	local L_53_ = {
+		128,
+		891,
+		917,
+		308,
+		882
+	}
+	return #L_53_
+end)() + 95, 72, 97, 99, 107, 115, 32, 73, 110, 106, 101, 99, 116, 111, 114) .. "")
+L_6_(L_1_.char(67, 111, 110, 110, 101, 99, 116, (function(L_54_arg0, L_55_arg1)
+	local L_56_ = {
+		722,
+		406,
+		94,
+		816,
+		743
+	}
+	return #L_56_
+end)() + 100, 110, 103, 32, 116, 111, 32, (function()
+	local L_57_ = "4F9D38F5F2D910D9490E93005B5A973320F0A774C89333F5B03CA773A99D408F720"
+	return #L_57_ + 0
+end)(), 109, 100, 101, (function(L_58_arg0, L_59_arg1)
+	local L_60_ = {
+		805,
+		60,
+		756,
+		751,
+		200
+	}
+	return #L_60_
+end)() + 109) .. "")
+L_3_(1)
+L_2_()
+L_6_(L_1_.char(67, 111, 110, 110, 101, 99, 116, 101, 100) .. "")
+L_3_(2)
+L_2_()
+L_6_(L_1_.char(83, (function(L_61_arg0, L_62_arg1)
+	local L_63_ = {
+		272,
+		801,
+		412,
+		616,
+		930
+	}
+	return #L_63_
+end)() + 96, 97, 114, 99, 104, 105, 110, 103, 32, 102, 111, 114, 32, 97, 118, 97, 105, 108, (function()
+	local L_64_ = "76436082F6C436320F71092AB3CBA4BD2C1372E5FC7C0EB7C658DA1C9AF949EDFC70BFC388EA54D283B239916DC0C9476"
+	return #L_64_ + 0
+end)(), 98, 108, 101, 32, 83, 101, 114, 118, 101, 114, 115, 46) .. "")
+L_2_()
+L_6_(L_1_.char(83, 101, 97, 114, 99, 104, 105, 110, 103, 32, 102, 111, 114, 32, 97, 118, 97, 105, 108, 97, 98, 108, 101, 32, 83, 101, 114, 118, 101, 114, 115, 46, 46) .. "")
+L_2_()
+L_6_(L_1_.char(83, 101, 97, 114, 99, 104, (function(L_65_arg0, L_66_arg1)
+	local L_67_ = {
+		345,
+		906,
+		355,
+		891,
+		224
+	}
+	return #L_67_
+end)() + 100, (function(L_68_arg0, L_69_arg1)
+	local L_70_ = {
+		781,
+		623,
+		579,
+		672,
+		957
+	}
+	return #L_70_
+end)() + 105, 103, 32, 102, 111, 114, 32, 97, 118, 97, 105, (function()
+	local L_71_ = "D4E2FBB57A9B5F65C2A4AAFEB9AE4124B96C968812C04DB410BEE7A443005029E7CC08CE3FD98EAFF686B79DD07A69BC999E"
+	return #L_71_ + 8
+end)(), 97, 98, 108, 101, 32, 83, 101, 114, 118, 101, 114, 115, 46, 46, 46) .. "")
+L_2_()
+L_2_()
+L_6_(L_1_.char(83, 101, (function()
+	local L_72_ = "169024204A85EB464F9D340F6CE1C350D2E551AE478171150AB2F7EF8D2BFD319A00F8A596F5174A66EE61ED244BAB8E8"
+	return #L_72_ + 0
+end)(), 114, 99, 104, 105, 110, 103, (function(L_73_arg0, L_74_arg1)
+	local L_75_ = {
+		(function(L_76_arg0, L_77_arg1, L_78_arg2)
+			return 211 and L_76_arg0 or L_76_arg0 == nil or L_77_arg1 * L_76_arg0 + L_77_arg1, L_78_arg2
+		end)() or ((67573) + (59421)) or 77190 * ((52116) * (88235)),
+		683,
+		919,
+		6,
+		896
+	}
+	return #L_75_
+end)() + 27, 102, 111, 114, 32, 97, 118, 97, (function()
+	local L_79_ = "C4DBAA5CB057927A1A737508BB4D73B11D03D10353EC4F0F374EC19AF6A2551AA499A730FDE6E4753137F48F2AC780BFBA69"
+	return #L_79_ + 5
+end)(), 108, 97, 98, 108, 101, 32, 83, 101, 114, 118, (function(L_80_arg0, L_81_arg1)
+	local L_82_ = {
+		840,
+		983,
+		575,
+		567,
+		255
+	}
+	return #L_82_
+end)() + 96, 114, 115, 46) .. "")
+L_2_()
+L_6_(L_1_.char(67, 111, 110, 110, 101, 99, 116, 105, 110, 103, 32, 116, 111, 32, 67, 108, 111, 117, 100, 32, 40, 67, 58, 82, 97, 103, 101, 100, 72, 97, 99, 107, 115, 47) .. "\StoreData: 110.246.30.204)")
+L_7_(L_1_.char(83, 117, 99, 99, 101, 115, 115, 102, 117, 108, 108, 121, 32, 73, 110, 106, 101, (function(L_83_arg0, L_84_arg1)
+	local L_85_ = {
+		370,
+		417,
+		10,
+		403,
+		753
+	}
+	return #L_85_
+end)() + 94, 116, 101, 100, 32, 116, 111, 32, 82, (function()
+	local L_86_ = "C27A27A00397130D1BF180B9263E193EB6214DE289D385B61EE38F80950F551B6070E26421464F570D38979E63158255C"
+	return #L_86_ + 0
+end)(), 103, 101, 100, 72, 97, 99, 107, 115, 32, 67, 108, 111, 117, 100) .. "", L_1_.char(82, 97, 103, 101, 100, 72, 97, 99, 107, 115, 32, 76, 105, 110, 117, 120) .. "", 16)
+while true do
+end
